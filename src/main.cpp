ca41c765ab8e378aa816d1e8edb096c1b30e1374
PTrefall@gmail.com <PTrefall@gmail.com@2c5777c1-dd38-1616-73a3-7306c0addc79>
2011-08-23 02:03:22 +0000

    First step. GraphicsView hello world integrated, tested and working :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 
-#include <QtGui>
-#include <QGLWidget>
+#include <QtGui/QApplication>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+	// making a scene on the application
+    QGraphicsScene scene;
+ 
+    //adding some text to the scene
+    scene.addText("Hello, world!", QFont("Times", 20, QFont::Bold));
+ 
+    //assigning that scene to the view
+    QGraphicsView view(&scene);
+    view.show();
+
     return app.exec();
 }
